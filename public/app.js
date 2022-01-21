a02d9b270357c7e2b06ef5a1ceae0b97c828af9f
Bam <andrefmartins92@hotmail.com>
2022-01-21 16:24:28 +0000

    Added rendering and search functions

--- public/app.js
+++ public/app.js
@@ -2,9 +2,11 @@
 const openWeatherKey = '<YOUR API KEY HERE>';
 const urlWeather = 'https://api.openweathermap.org/data/2.5/weather';
 
+
 // Foursquare API info
 const foursquareKey = '<YOUR API KEY HERE>';
 const url = 'https://api.foursquare.com/v3/places/search?near=';
+
 
 // Page Elements
 const $input = $('#city');
@@ -19,9 +21,11 @@ const options = {
     method: 'GET',
     headers: {
         Accept: 'application/json',
-        Authorization: '<YOUR FOURSQUARE API KEY HERE>' 
+        Authorization: foursquareKey,
+        crossOrigin: true
     }
 };
+
 
 // AJAX Functions 
 
@@ -41,7 +45,7 @@ const getPlaces = async () => {
 }
 
 const getWeather = async () => {
-    const urlToFetch = `${urlWeather}?q=${input.val()}&APPID=${openWeatherKey}`;
+    const urlToFetch = `${urlWeather}?q=${$input.val()}&APPID=${openWeatherKey}`;
     try {
         const response = await fetch(urlToFetch);
         if (response.ok) {
@@ -53,17 +57,40 @@ const getWeather = async () => {
     }
 }
 
+
 // Render functions
+ 
+const renderPlaces = (places) => {
+    $placeDivs.forEach(($place, index) => {
+        const place = places[index];
+        const placeIcon = place.categories[0].icon;
+        const placeImgSrc = `${placeIcon.prefix}bg_64${placeIcon.suffix}`;
+        const placeContent = createPlaceHTML(place.name, place.location, placeImgSrc);
+        $place.append(placeContent);
+    });
+    $destination.append(`<h2>${places[0].location.locality}</h2>`);
+}
+
+
+const renderWeather = (forecast) => {
+    const weatherContent = createWeatherHTML(forecast);
+    $weatherDiv.append(weatherContent);
+} 
 
 
 // Search function
+
 const executeSearch = () => {
     $placeDivs.forEach(place => place.empty());
     $weatherDiv.empty();
     $destination.empty();
     $container.css("visibility", "visible");
-    getPlaces();
-    getWeather();
+    getPlaces().then(places => {
+        return renderPlaces(places);
+    });
+    getWeather().then(forecast => {
+        return renderWeather(forecast);
+    });
     return false;
 }
 
